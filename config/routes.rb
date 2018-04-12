b245d34f5ccee4c10dab86ec201f617d82acfa79
hungmi <hungmihungmi@gmail.com>
2018-04-13 00:05:35 +0800

    add resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get 'admin', to: redirect('/')
-namespace :admin do
-  resources :users
-  get 'signin', to: 'sessions#new'
-  post 'signin', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-end
+	get 'admin', to: redirect('/admin/workpieces')
+	namespace :admin do
+		resources :customers do
+			resources :workpieces
+		end
+		resources :workpieces do
+			resources :manufacturing_records
+		end
+		resources :factories do
+			resources :procedures
+		end
+		resources :manufacturing_records
+	  resources :users
+	  get 'signin', to: 'sessions#new'
+	  post 'signin', to: 'sessions#create'
+	  delete 'logout', to: 'sessions#destroy'
+	end
 end
